4fbcff4bde4de6b30ec54681dcddcc4818746646
f-westergren <folke.westergren@gmail.com>
2020-07-26 17:05:14 -0400

    add rawBodySaver

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const iRoute = require('./routes');
 const bodyParser = require('body-parser');
 
-// app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ 
+  extended: true,
+  verify: rawBodySaver
+ }))
+ 
 app.use('/', iRoute)
 
 app.use('*', (req, res) => {
@@ -14,3 +17,11 @@ app.use('*', (req, res) => {
 app.listen(3000, () => {
   console.log('Server starting on port 3000.')
 })
+
+
+// Get raw request body.
+function rawBodySaver (req, res, buf, encoding) {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || 'utf8')
+  }
+}
